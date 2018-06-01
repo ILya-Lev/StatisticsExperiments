56c683aa933fd10194f0189ac52d7d0769b8f24e
Illia Levandovskyi <lis2889@gmail.com>
2018-06-01 18:41:33 +0300

    Add project files.

---
+++ FellerProbability/FourNumbersSum.cs
@@ -0,0 +1,26 @@
+﻿namespace FellerProbability
+{
+    public class FourNumbersSum
+    {
+        public int GetAmount(int threshold = 100)
+        {
+            var amount = 0;
+            for (int a = 0; a <= threshold; a++)
+            {
+                for (int b = 0; b <= threshold; b++)
+                {
+                    for (int c = 0; c <= threshold; c++)
+                    {
+                        var d = threshold - a - b - c;
+                        if (d >= 0 && d <= threshold)
+                        {
+                            ++amount;
+                        }
+                    }
+                }
+            }
+
+            return amount;
+        }
+    }
+}
