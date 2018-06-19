91c32bc4363f72b6fdea31dcca62aeb28fbfb457
Illia Levandovskyi <lis2889@gmail.com>
2018-06-20 00:59:21 +0300

    heap as an array implementation

---
+++ FellerProbability/DataStructures/Heap.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FellerProbability.DataStructures
+{
+    public abstract class Heap<T> where T : IComparable<T>
+    {
+        private readonly List<T> _storage = new List<T>();
+
+        public int Count => _storage.Count;
+
+        public T Peek() => _storage.FirstOrDefault();
+
+        public void Push(T item)
+        {
+            _storage.Add(item);
+            BubbleUp();
+        }
+
+        public T Pop()
+        {
+            var head = Peek();
+            Swap(0, _storage.Count - 1);
+            _storage.RemoveAt(_storage.Count - 1);
+            BubbleDown();
+            return head;
+        }
+
+        private void BubbleUp()
+        {
+            var childIdx = _storage.Count - 1;
+            var parentIdx = GetParentIndex(childIdx);
+            while (parentIdx.HasValue && !ObeysHeapProperty(_storage[parentIdx.Value], _storage[childIdx]))
+            {
+                Swap(parentIdx.Value, childIdx);
+
+                childIdx = parentIdx.Value;
+                parentIdx = GetParentIndex(childIdx);
+            }
+        }
+
+        private void BubbleDown()
+        {
+            var parentIdx = 0;
+            var childIdx = GetProperChildIdx(parentIdx);
+            while (childIdx.HasValue && !ObeysHeapProperty(_storage[parentIdx], _storage[childIdx.Value]))
+            {
+                Swap(parentIdx, childIdx.Value);
+
+                parentIdx = childIdx.Value;
+                childIdx = GetProperChildIdx(parentIdx);
+            }
+        }
+
+        protected abstract bool ObeysHeapProperty(T parent, T child);
+
+        private void Swap(int parentIdx, int childIdx)
+        {
+            var tmp = _storage[parentIdx];
+            _storage[parentIdx] = _storage[childIdx];
+            _storage[childIdx] = tmp;
+        }
+
+        private int? GetParentIndex(int childIndex)
+        {
+            var parentIndex = (childIndex - 1) / 2;
+            return parentIndex >= 0 ? parentIndex : default(int?);
+        }
+
+        private int? GetProperChildIdx(int parentIndex)
+        {
+            var firstChildIndex = parentIndex * 2 + 1;
+            if (firstChildIndex >= _storage.Count)
+                return null;
+
+            var secondChildIndex = firstChildIndex + 1;
+            if (secondChildIndex >= _storage.Count)
+                return firstChildIndex;
+
+            return ObeysHeapProperty(_storage[firstChildIndex], _storage[secondChildIndex])
+                ? firstChildIndex
+                : secondChildIndex;
+        }
+    }
+
+    public class MaxHeap<T> : Heap<T> where T : IComparable<T>
+    {
+        protected override bool ObeysHeapProperty(T parent, T child) => parent.CompareTo(child) >= 0;
+    }
+
+    public class MinHeap<T> : Heap<T> where T : IComparable<T>
+    {
+        protected override bool ObeysHeapProperty(T parent, T child) => parent.CompareTo(child) <= 0;
+    }
+}
---
+++ FellerProbabilityTests/HeapTests/MinHeapTests.cs
@@ -0,0 +1,63 @@
+﻿using FellerProbability.DataStructures;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+using Xunit.Sdk;
+
+namespace FellerProbabilityTests.HeapTests
+{
+    public class MinHeapTests : IClassFixture<TestOutputHelper>
+    {
+        private readonly TestOutputHelper _outputHelper;
+
+        public MinHeapTests(TestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Push_Sample_ShouldBeAsExpected()
+        {
+            var heap = new MinHeap<int>();
+            var vals = new[] { 4, 4, 8, 9, 4, 12, 9, 13, 11 };
+            for (int i = 0; i < vals.Length; i++)
+            {
+                heap.Push(vals[i]);
+                heap.Peek().Should().Be(4, $"after inserting {i}th value {vals[i]}");
+            }
+        }
+
+        [Fact]
+        public void Push_OrderedSample_ShouldBeAsExpected()
+        {
+            var heap = new MinHeap<int>();
+            var vals = new[] { 4, 4, 8, 9, 4, 12, 9, 13, 11 }.OrderByDescending(n => n).ToArray();
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                heap.Push(vals[i]);
+            }
+
+            heap.Peek().Should().Be(4);
+        }
+
+        [Fact]
+        public void Pop_Sample_ShouldBeAsExpected()
+        {
+            var heap = new MinHeap<int>();
+            var vals = new[] { 4, 4, 8, 9, 4, 12, 9, 13, 11 };
+            foreach (var val in vals)
+            {
+                heap.Push(val);
+            }
+
+            for (var head = heap.Pop(); ; head = heap.Pop())
+            {
+                _outputHelper.WriteLine($"{head}");
+                if (heap.Count == 0)
+                    break;
+            }
+        }
+
+    }
+}
