90da33390373e6785b809876d6929792dc8a932a
Illia Levandovskyi <lis2889@gmail.com>
2018-07-14 17:03:52 +0300

    find unpaired odd integer in an array of paired odd integers wired thing - in problem it's expected the solution be O(N) but O(N log N) solution passes final tests

---
+++ Codility.UnitTests/OddOccurrencesInArrayTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Codility.UnitTests
+{
+    public class OddOccurrencesInArrayTests
+    {
+        [Fact]
+        public void GetUnpairedOddInteger_UnpairedNumberPresent_Find()
+        {
+            var solver = new OddOccurrencesInArray();
+            var source = new[] { 1, 5, 1, 3, 1, 7, 3, 5, 1 };
+
+            var unpairedNumber = solver.GetUnpairedOddInteger(source);
+
+            unpairedNumber.Should().Be(7);
+        }
+
+        [Fact]
+        public void GetUnpairedOddInteger_Triplet_Find()
+        {
+            var solver = new OddOccurrencesInArray();
+            var source = new[] { 1, 5, 1, 3, 3, 5, 1 };
+
+            var unpairedNumber = solver.GetUnpairedOddInteger(source);
+
+            unpairedNumber.Should().Be(1);
+        }
+    }
+}
---
+++ Codility/OddOccurrencesInArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility
+{
+    public class OddOccurrencesInArray
+    {
+        public int GetUnpairedOddInteger(int[] source)
+        {
+            var ordered = source.OrderBy(n => n);   //already O(N logN) - we need O(N)
+            var total = 0;
+            var sign = 1;
+            foreach (var number in ordered)
+            {
+                total += sign * number;
+                sign = sign > 0 ? -1 : 1;
+            }
+
+            return Math.Abs(total);
+        }
+    }
+}
