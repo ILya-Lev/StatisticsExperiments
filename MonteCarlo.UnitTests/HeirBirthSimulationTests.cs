d56f9f837dc22a1b7a920a252b9d373d49a91043
Illia Levandovskyi <lis2889@gmail.com>
2018-07-15 18:46:19 +0300

    birth rate for strategy - do girls until a boy is born

---
+++ MonteCarlo.UnitTests/HeirBirthSimulationTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace MonteCarlo.UnitTests
+{
+    public class HeirBirthSimulationTests
+    {
+        [InlineData(1000)]
+        [InlineData(10_000)]
+        [InlineData(100_000)]
+        [InlineData(1_000_000)]
+        [Theory]
+        public void Simulate_Many_FiftyFifty(int amountOfFamilies)
+        {
+            var simulation = new HeirBirthSimulation(50, 20);
+            var rate = simulation.Simulate(amountOfFamilies);
+
+            var precision = 3 / Math.Sqrt(amountOfFamilies);
+            rate.Should().BeApproximately(1.0, precision);
+        }
+    }
+}
---
+++ MonteCarlo/HeirBirthSimulation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonteCarlo
+{
+    public class HeirBirthSimulation
+    {
+        private readonly int _boysThreshold;
+        private readonly int _maxKidsInOneFamily;
+        private static readonly Random _random = new Random(DateTime.Now.Millisecond);
+
+        public HeirBirthSimulation(int boysThreshold = 50, int maxKidsInOneFamily = 10)
+        {
+            _boysThreshold = boysThreshold;
+            _maxKidsInOneFamily = maxKidsInOneFamily;
+        }
+
+        public double Simulate(int familyNumber)
+        {
+            var children = Enumerable.Range(1, familyNumber)
+                            .SelectMany(family => GenerateChildSequence())
+                            .ToArray();
+
+            var boys = children.Count(child => child == true);
+            var girls = children.Count(child => child == false);
+
+            return 1.0 * girls / boys;
+        }
+
+        private IEnumerable<bool> GenerateChildSequence()
+        {
+            for (int numberOfKid = 0; numberOfKid < _maxKidsInOneFamily; numberOfKid++)
+            {
+                if (IsBoy(_random.Next(1, 100)))
+                {
+                    yield return true;
+                    yield break;
+                }
+                yield return false;
+            }
+        }
+
+        private bool IsBoy(int next) => next >= _boysThreshold;
+    }
+}
