d17ba6b617cba2cf6f48cd7cd916676bfd0dcab6
Illia Levandovskyi <lis2889@gmail.com>
2018-07-15 18:13:07 +0300

    needle throw simulation to calculate pi value

---
+++ MonteCarlo.UnitTests/NeedleThrowingSimulationTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace MonteCarlo.UnitTests
+{
+    public class NeedleThrowingSimulationTests : IClassFixture<TestOutputHelper>
+    {
+        private readonly TestOutputHelper _outputHelper;
+
+        public NeedleThrowingSimulationTests(TestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [InlineData(10_000)]
+        [InlineData(100_000)]
+        [Theory]
+        public void Simulate_Many_PiDividedByTen(int amount)
+        {
+            var simulation = new NeedleThrowingSimulation();
+
+            var probability = simulation.Simulate(amount);
+
+
+            _outputHelper.WriteLine($"{probability * 10}");
+            var precision = 1 / Math.Sqrt(amount);
+            probability.Should().BeInRange(Math.PI / 10 - precision, Math.PI / 10 + precision);
+        }
+    }
+}
---
+++ MonteCarlo/NeedleThrowingSimulation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MonteCarlo
+{
+    public class NeedleThrowingSimulation
+    {
+        private static readonly Random _random = new Random(DateTime.Now.Millisecond);
+        private const int Size = 1000;
+        private const int Distance = 10;
+
+        private readonly List<double> _lines = new List<double>();
+
+        public NeedleThrowingSimulation()
+        {
+            InitializeLines();
+        }
+
+        public double Simulate(int amount)
+        {
+            var intersections = 0;
+            for (int i = 0; i < amount; i++)
+            {
+                var needle = ThrowNeedle();
+
+                var intersects = _lines.Any(ln => needle.IntersectsHorizontalLine(ln));
+                if (intersects)
+                    intersections++;
+            }
+
+            return intersections * 1.0 / amount;
+        }
+
+        private Needle ThrowNeedle()
+        {
+            return new Needle(length: GetRandom(Distance), orientation: GetRandom(2 * Math.PI), x: GetRandom(Size), y: GetRandom(Size));
+        }
+
+        private void InitializeLines()
+        {
+            for (int i = 0; i < Size / Distance; i++)
+            {
+                _lines.Add(i * Distance);
+            }
+        }
+
+        private static double GetRandom(double limit) => limit * _random.NextDouble();
+    }
+}
